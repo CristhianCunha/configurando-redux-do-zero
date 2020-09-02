56e08663ba5f6bfd6552051b54187137b536e64a
Cristhian Cunha <cristhiancunha@cc.ci.ufpb.br>
2020-09-02 02:34:57 -0300

    V6.1  restrict use to numbers in FormCount
    Restringir o formulário  em 'FormCount' ao uso de números inteiros

--- src/FormCount.js
+++ src/FormCount.js
@@ -6,7 +6,6 @@ import { selectors } from './redux/selectors/count'
 
 const FormCount = () => {
   const [value, updateValue] = useState(0)
-  let addsub = 1
 
   const dispatch = useDispatch()
   const count = useSelector(selectors.getCount)
@@ -17,12 +16,12 @@ const FormCount = () => {
 
   const multiplierValue = (multiplier) => {
     updateValue(value*multiplier)
+    
   }
 
   const handleFormSubmit = (event) => {
     event.preventDefault()
     //se não houver nada escrito, parar função
-
     dispatch(actions.addValue(value))
     updateValue(0)
   }
@@ -32,7 +31,9 @@ const FormCount = () => {
     <>
       <h2>{count}</h2>
       <form onSubmit={handleFormSubmit}>
-        <input onChange={handleInputChange} value={value}/>
+        <input type="text" pattern="[0-9]*" 
+          onChange={handleInputChange}
+          value={value}/>
         <button onClick={() => multiplierValue(1)} >Adicionar Valor</button>
         <button onClick={() => multiplierValue(-1)} >Subtrair Valor</button>
       </form>
